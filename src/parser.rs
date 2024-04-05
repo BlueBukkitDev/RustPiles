5c66ce2159883ff807dc5edd6f184fd1b32b8bc7
Jonathan Bruce <bluebukkitdev@gmail.com>
2024-04-05 09:52:10 -0400

    Resolved all errors.
     Now properly reads and prints all lines while parsing into Words.

--- src/parser.rs
+++ src/parser.rs
@@ -5,40 +5,45 @@ struct Word {
 }
 
 impl Word {
-    pub fn new(&self, text: &str) -> Word {
+    pub fn new(text: &str) -> Word {
         Word {
-            content: String::from(&text),
-            kind: Word::getKind(text)
+            content: String::from(text),
+            kind: Word::get_kind(text)
         }
     }
     ///Gets a Vec of Words from a line of text.
     pub fn get_words(line: String) -> Vec<Word> {
-        let chars = line.chars().collect();
-        let mut words = Vec::new();
-        let mut builder = "";
+        let chars:Vec<char> = line.chars().collect();
+        let mut words:Vec<Word> = Vec::new();
+        let mut builder = String::new();
         let mut whitespace = false;
         let mut i = 0;
 
         while i < chars.len() {
-            if chars[i] == " " {
+            if chars[i] == ' ' {
                 whitespace = true;
             } else {
                 whitespace = false;
-                builder = builder + chars[i];
+                builder.push(chars[i]);
             }
-            if Word::get_kind(builder) == WordType::Symbol {
-                if Word::get_kind(builder + chars[i + 1]) == WordType::Symbol {
-                    words.add(builder);
-                    builder = "";
+            if Word::get_kind(&builder.clone()) == WordType::Symbol {
+                let mut test = builder.clone();
+                test.push(chars[i+1]);
+                if Word::get_kind(&test) == WordType::Symbol {
+                    words.push(Word::new(&builder.clone()));
+                    builder = String::new();
                     i += 2;
                     continue;
                 }
-                words.add(builder);
+                words.push(Word::new(&builder.clone()));
                 i += 1;
                 continue;
-            } else if Word::get_kind(builder) == WordType::Keyword {}
+            } else if Word::get_kind(&builder.clone()) == WordType::Keyword {
+
+            }
             i += 1;
         }
+        return words;
     }
 
     pub fn get_kind(text:&str) -> WordType {
@@ -50,6 +55,7 @@ impl Word {
     }
 }
 
+#[derive(PartialEq)]
 enum WordType{
     Keyword, Symbol, UserDefined
 }
